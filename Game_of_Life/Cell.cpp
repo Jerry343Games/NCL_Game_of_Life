512ababa5a2f95bffed2b3edc6c48e59fdf5ba9a
Jerry <75481387+Jerry343Games@users.noreply.github.com>
2024-10-15 11:09:17 +0100

    Fix Cell.pp

--- Game_of_Life/Cell.cpp
+++ Game_of_Life/Cell.cpp
@@ -1,23 +1,23 @@
 #include "Cell.h"
 #include <iostream>
 
-// 默认构造函数
-Cell::Cell() : state(0) {}  // 默认情况下，状态为死细胞
+// Default constructor
+Cell::Cell() : state(0) {}  // By default, the state is a dead cell
 
-// 带初始状态的构造函数
+// Constructor with initial state
 Cell::Cell(int initState) : state(initState) {}
 
-// 获取细胞状态
+// Get cell state
 int Cell::getState() const {
     return state;
 }
 
-// 设置细胞状态
+// Set cell state
 void Cell::setState(int newState) {
     state = newState;
 }
 
-// 打印细胞状态
+// Print cell state
 void Cell::printCell() const {
     std::cout << (state == 1 ? "Alive" : "Dead") << std::endl;
 }
